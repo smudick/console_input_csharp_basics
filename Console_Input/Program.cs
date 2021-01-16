789d80bb82af68f464c64d79d8a3493a3fd116df
Sam Mudick <smudick@gmail.com>
2021-01-15 22:19:01 -0600

    Console input

---
+++ Console_Input/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Console_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console Input
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            var randomAnimal = new Random().Next(animals.Length);
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! What is your favorite color?");
+            var favColor = Console.ReadLine();
+            Console.WriteLine($"Would you like a {favColor} {animals[randomAnimal]}?");
+
+        }
+    }
+}
